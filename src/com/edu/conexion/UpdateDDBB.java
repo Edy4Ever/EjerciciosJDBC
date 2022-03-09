0139d11fdc936400b0f217351b0bfa18c4f90a53
eduardo <4eversmile52@gmail.com>
2022-03-09 15:45:21 -0600

    Creamos el archivo java para actualizar datos en la DDBB

---
+++ src/com/edu/conexion/UpdateDDBB.java
@@ -0,0 +1,29 @@
+package com.edu.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class UpdateDDBB {
+
+	public static void main(String[] args) {
+		
+		try {
+			Connection myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ejemplos", "root", "");
+
+			Statement myStatement = myConnection.createStatement();
+
+			String SqlInstruction = "UPDATE datos SET Edad=60 WHERE Nombre='Eugenio'";
+			
+			int filaAfectada = myStatement.executeUpdate(SqlInstruction);
+			
+			System.out.println("Registro actualizado correctamente");
+			System.out.println("Fila agregada " + filaAfectada);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
